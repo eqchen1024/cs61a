ee492ddd4b8b05380cacf770cd461a8653ec55d5
eqchen1024 <cdysbt@gmail.com>
2021-08-22 21:46:19 +0800

    8/22

--- lab/lab07/lab07_extra.py
+++ lab/lab07/lab07_extra.py
@@ -18,25 +18,12 @@ def remove_all(link , value):
     <0 1>
     """
     "*** YOUR CODE HERE ***"
-    '''
-    错误写法
-    这样递归做导致的结果就是修改只在每次递归传入的部分copied链表上进行并没有修改在原链表上
-    if link is Link.empty:
-        return
-    else:
-        if link.first==value:
-            link=link.rest
-        print(link)
-        remove_all(link.rest,value)
-    '''
-    while link.rest:
-        while link.second==value:
-            link.rest=link.rest.rest
-            if link.rest is link.empty:
-                return
-        link=link.rest
-
-
+    cur=link
+    while cur.rest is not link.empty:
+        if cur.rest.first==value:
+            cur.rest=cur.rest.rest
+        else:
+            cur=cur.rest
 # Q7
 def deep_map_mut(fn, link):
     """Mutates a deep link by replacing each item found with the
@@ -51,14 +38,14 @@ def deep_map_mut(fn, link):
     <9 <16> 25 36>
     """
     "*** YOUR CODE HERE ***"
-    while link.rest:
-        while isinstance(link.first,Link):
-            link=link.first
-            fn(link.first)
-            link=link.rest
-        fn(link.first)
-        link=link.rest
-
+    cur=link #generate a moving cursor and keep the head pointer untouched
+    if cur is Link.empty:
+        return
+    if isinstance(cur.first,int):
+        cur.first=fn(cur.first)
+    elif isinstance(cur.first,Link):
+        deep_map_mut(fn,cur.first)
+    deep_map_mut(fn,cur.rest)
 
 # Q8
 def has_cycle(link):
@@ -76,6 +63,14 @@ def has_cycle(link):
     False
     """
     "*** YOUR CODE HERE ***"
+    log=set()
+    cur=link
+    while cur is not Link.empty:
+        log.add(cur)
+        cur=cur.rest
+        if cur in log:
+            return True
+    return False
 
 def has_cycle_constant(link):
     """Return whether link contains a cycle.
@@ -89,9 +84,21 @@ def has_cycle_constant(link):
     False
     """
     "*** YOUR CODE HERE ***"
+    # fast slow cursor
+    slow=link
+    fast=slow.rest
+    # 注意有cycle的链表永远不会结束 快指针每步多走1step 等套圈了自然就会相遇
+    while fast:
+        if fast != slow:
+            fast,slow=fast.rest.rest,slow.rest
+        else:
+            return True
+    return False
+
+
 
 # Q9
-def reverse_other(t):
+def reverse_other(t,depth=0):
     """Mutates the tree such that nodes on every other (even_indexed) level
     have the labels of their branches all reversed.
 
@@ -105,3 +112,17 @@ def reverse_other(t):
     Tree(1, [Tree(8, [Tree(3, [Tree(5), Tree(4)]), Tree(6, [Tree(7)])]), Tree(2)])
     """
     "*** YOUR CODE HERE ***"
+    if depth%2==1:
+        pass
+    else:
+        if len(t.branches)==0:
+            return
+        name_pool=[]
+        for i in t.branches:
+            name_pool.append(i.label)
+        name_pool=name_pool[::-1]
+        for i in range(len(t.branches)):
+            t.branches[i].label=name_pool[i]
+    depth+=1
+    for i in t.branches:
+        reverse_other(i,depth)
